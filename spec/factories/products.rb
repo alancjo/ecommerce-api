80e3d74a0e26e44874dd48fed7808bd9a870523d
Alan Oliveira <alancjobm@gmail.com>
2022-01-22 01:03:17 -0300

    Criar model polimorfica de produto

---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product do
+    sequence(:name) { |n| "Basic #{n}" }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Commerce.price(range: 100.00..400.00) }
+    productable { nil }
+  end
+end
