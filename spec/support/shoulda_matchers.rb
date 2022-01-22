094fdc2090a568ee8cbcc76cb7e0d18858c4ad89
Alan Oliveira <alancjobm@gmail.com>
2022-01-21 23:53:25 -0300

    Criar model Category, criar validacoes e seus respectivos testes

--- spec/support/shoulda_matchers.rb
+++ spec/support/shoulda_matchers.rb
@@ -1,4 +1,6 @@
 Shoulda::Matchers.configure do |config|
-  with.test_framework :rspec
-  with.library :rails
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
